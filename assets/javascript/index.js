58c64874c06f87bf9f148a0af82c1b92aeae9c1d
fredrikj31 <fredrik1206@gmail.com>
2020-09-23 19:34:57 +0200

    Updated with bootstrap

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -5,3 +5,16 @@ var element = document.querySelector("#graph-container");
 panzoom(element, {
 	smoothScroll: false,
 });
+
+// Sidebar Toggler
+$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+	if ($("#wrapper").hasClass("toggled") == true) {
+		$("#toggleArrow").removeClass("fa-arrow-left");
+		$("#toggleArrow").addClass("fa-arrow-right");
+	} else {
+		$("#toggleArrow").removeClass("fa-arrow-right");
+		$("#toggleArrow").addClass("fa-arrow-left");
+	}
+  });
